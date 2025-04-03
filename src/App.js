1989abfd71d51060f951a2319b7c6e878fe41685
Christabelle143 <christabelleyouta143@gmail.com>
2025-04-03 13:22:09 +0100

    add search , sort and add to favorite on the favorite section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import DisplayCryptoList from './components/CryptoList';
+import CryptoList from './components/CryptoList';
 
 function App() {
   return (
     <div className="App">
       <h1>Cryptocurrency Dashboard</h1>
-      <DisplayCryptoList />
+      <CryptoList />
     </div>
   );
 }
